6286f0608e78f432d14071c1d80cc7943dec8c53
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2019-09-07 19:09:23 -0300

    Pelotita que se mueve

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        GetComponent<Transform>().position = player.GetComponent<Transform>().position - new Vector3(0,-4.5F,10);
+        // GetComponent<Transform>().position = player.GetComponent<Transform>(). - new Vector3(10,-10,10);
+    }
+}
---
+++ Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+
+    public float accX = 0;
+    public float accY = 0;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        accY = Input.GetAxis("Vertical");
+        accX =  Input.GetAxis("Horizontal");
+
+        float newX = Mathf.Clamp(accX + GetComponent<Rigidbody>().velocity.x,-10,10);
+        float newY = Mathf.Clamp(accY + GetComponent<Rigidbody>().velocity.z,-10,10);
+
+        GetComponent<Rigidbody>().velocity = new Vector3(newX, GetComponent<Rigidbody>().velocity.y , newY);
+    }
+}

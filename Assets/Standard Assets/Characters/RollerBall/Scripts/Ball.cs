fe3e5dc2c597f2324b432364eada6e8dbf1f735b
matuteale <mbuscagl@itba.edu.ar>
2019-11-18 19:43:45 -0300

    Improve ball physics

--- Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
@@ -29,12 +29,13 @@ namespace UnityStandardAssets.Vehicles.Ball
             {
                 // ... add torque around the axis defined by the move direction.
                 m_Rigidbody.AddTorque(new Vector3(moveDirection.z, 0, -moveDirection.x)*m_MovePower);
-            }
-            else
-            {
-                // Otherwise add force in the move direction.
                 m_Rigidbody.AddForce(moveDirection*m_MovePower);
             }
+            // else
+            // {
+            //     // Otherwise add force in the move direction.
+            //     m_Rigidbody.AddForce(moveDirection*m_MovePower);
+            // }
 
             // If on the ground and jump is pressed...
             if (Physics.Raycast(transform.position, -Vector3.up, k_GroundRayLength) && jump)

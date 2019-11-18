6a88651151ecdfbe6428439e932ad730e240add0
Impervius <nicodonof@hotmail.com>
2019-11-18 19:57:25 -0300

    Bien ahora sin delete

---
+++ Assets/ForeverRoll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForeverRoll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(-1,2,-0.5F); 
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.anyKey) {
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        }
+    }
+}
